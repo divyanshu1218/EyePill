2969eec853e17c1bb123c6997a5b57c88c8547f5
divyanshu1218 <divyanshupeswani@gmail.com>
2026-05-17 14:58:25 +0530

    Statically imported mysql2 to force Vercel bundling

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('mysql2'); // Force Vercel to bundle the mysql2 driver
 const cors = require('cors');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
